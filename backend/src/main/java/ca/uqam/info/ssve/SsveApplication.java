10684288e677dec7aa06d85211ac89db7b8bc460
Zachary Peloquin <ck791917@ens.uqam.ca>
2022-10-26 21:24:10 -0400

    better fix for cors issue

--- backend/src/main/java/ca/uqam/info/ssve/SsveApplication.java
+++ backend/src/main/java/ca/uqam/info/ssve/SsveApplication.java
@@ -2,7 +2,9 @@ package ca.uqam.info.ssve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:3000")
 @SpringBootApplication
 public class SsveApplication {
 
