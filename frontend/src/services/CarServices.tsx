eb62bd021302158891b34301932d871f2cc24eea
Axel Sally <be991046@ens.uqam.ca>
2022-11-15 16:04:28 -0500

    refactor: utilisation d'axios pour la communication

--- frontend/src/services/CarServices.tsx
+++ frontend/src/services/CarServices.tsx
@@ -1,30 +1,24 @@
+import axios from "../http-commons";
+
 /**
  * These are all examples of services, if you need more complex
  * services feel free to create your own in it own file to prevent clutter.
  * Note that there is no error handling here but axios includes a elegant way to handle errors
- * that should be implemented in services. See https://axios-http.com/docs/handling_errors 
+ * that should be implemented in services. See https://axios-http.com/docs/handling_errors
  */
-const url = "http://localhost:8080/api"
 const getAllCars = async () => {
-  try{
-    const res = await fetch(`${url}/vehicle/evaluate`,
-    {
-      method: 'GET',
-    });
-  
-    const data = await res.json();
+  try {
+    const res = await axios.get("/vehicle/evaluate");
+    const data = await res.data;
     return data;
-  }
-  catch(err){
+  } catch (err) {
     console.log(err);
     return [];
   }
-
 };
 
-
 const CarServices = {
-  getAllCars
+  getAllCars,
 };
 
 export default CarServices;
