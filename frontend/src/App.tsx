d694ff1c814471bc64935508da7a5c4d7c344db1
Kim Joziak <cf991907@ens.uqam.ca>
2022-11-15 18:42:01 -0500

    retrait components inutiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 
 import logo from "./assets/onlycars_old.png";
-import { Card } from "./components/example/example";
-import AddCar from "./components/example2/example2";
-import CarList from './components/example3/example3';
 import Trip from "./components/trip/trip"
 import MakeResearch from "./components/makeResearch/makeResearch";
 
@@ -18,9 +15,6 @@ function App() {
 
       <div className={"App-body"}>
         <Routes>
-          <Route path="/exemple" element={<Card title="Titre exemple" paragraph='Ceci est un exemple' />} />
-          <Route path="/add" element={<AddCar />} />
-          <Route path="/get" element={<CarList />} />
           <Route path="/researchMan" element={<Trip />} />
           <Route path="/" element={<MakeResearch />} />
         </Routes>
