4dd70fb7b197fc7f0b7f00cee46cd8a6de74de73
Kim Joziak <cf991907@ens.uqam.ca>
2022-11-15 22:54:19 -0500

    retrait classes inutiles

--- backend/src/main/java/ca/uqam/info/ssve/repository/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package ca.uqam.info.ssve.repository;
-
-import ca.uqam.info.ssve.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-
-}
