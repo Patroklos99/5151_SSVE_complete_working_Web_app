517348bb2bcbe91300840281c360f501c88e5c15
Zachary Peloquin <ck791917@ens.uqam.ca>
2022-12-03 18:06:02 -0500

    fix temporaire en attendant que les variable d'environnement soit foncitonnel

--- frontend/src/http-commons.tsx
+++ frontend/src/http-commons.tsx
@@ -5,11 +5,13 @@ import process from "process";
  * This is the configuration file for axios
  * modify it at your own risk
  */
-
-let development = process.env.NODE_ENV !== 'production'
-
 export default axios.create({
-  baseURL: String(process.env.REACT_APP_BASE_URL),
+  /**
+  baseURL: String(process.env.NODE_ENV) == "production"
+    ? process.env.REACT_APP_BASE_URL
+    : process.env.DEV_REACT_APP_BASE_URL,
+  */
+  baseURL: "http://adve.info.uqam.ca/api",
   headers: {
     "Content-Type": "application/json"
   }
