e3073f57867596eeec7c48ee502289332c21b5fd
Zachary Peloquin <ck791917@ens.uqam.ca>
2022-12-03 17:28:31 -0500

    ajout de variable d'environnement

--- frontend/src/http-commons.tsx
+++ frontend/src/http-commons.tsx
@@ -1,11 +1,15 @@
 import axios from "axios";
+import process from "process";
 
 /**
  * This is the configuration file for axios
  * modify it at your own risk
  */
+
+let development = process.env.NODE_ENV !== 'production'
+
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: String(process.env.REACT_APP_BASE_URL),
   headers: {
     "Content-Type": "application/json"
   }
