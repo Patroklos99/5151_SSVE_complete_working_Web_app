c71c95037dd7bc3a8f86859ce76f9177fe1ecd73
Maxime <max77oue@gmail.com>
2022-12-05 19:36:18 -0500

    fix merge

--- backend/src/main/java/ca/uqam/info/ssve/service/VehicleService.java
+++ backend/src/main/java/ca/uqam/info/ssve/service/VehicleService.java
@@ -3,18 +3,12 @@ package ca.uqam.info.ssve.service;
 import ca.uqam.info.ssve.model.*;
 import ca.uqam.info.ssve.repository.VehicleRepository;
 import ca.uqam.info.ssve.server.ADVEConnection;
-import com.jcraft.jsch.JSchException;
-import ca.uqam.info.ssve.model.Deplacement;
-import ca.uqam.info.ssve.model.Route;
 import ca.uqam.info.ssve.model.Evaluation;
 import ca.uqam.info.ssve.model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.*;
 
 @Service
@@ -217,52 +211,8 @@ public class VehicleService {
         // return vehicleFinalScore;
     }
 
-    private String requeteString(Route route) {
-        String start = "(" + route.getDeplacement().getStart().getLat() + ","
-                + route.getDeplacement().getStart().getLgt() + ")";
-        String end = "(" + route.getDeplacement().getEnd().getLat() + "," + route.getDeplacement().getEnd().getLgt()
-                + ")";
-        return start + " " + end + " ";
-    }
-
-    private void evaluateRoute(Route route, List<Vehicle> vehicle, int i) {
-        double poid1 = 0.75;
-        double poid2 = 0.25;
-        double note1 = getPercentage(vehicle.get(i).getElectricalCapacity(), route.getDistance());
-        if (note1 > 100)
-            note1 = 100;
-        int rangeMax = 0;
-        for (int j = 0; j < vehicle.size(); j++) {
-            if (vehicle.get(j).getElectricalCapacity() > rangeMax) {
-                rangeMax = vehicle.get(j).getElectricalCapacity();
-            }
-        }
-        double note2 = getPercentage((vehicle.get(i).getElectricalCapacity() - route.getDistance()),
-                (rangeMax - route.getDistance()));
-        if (note2 > 100) {
-            note2 = 100;
-        } else if (note2 < 0) {
-            note2 = 0;
-        }
-        route.setScore(poid1 * note1 + poid2 * note2);
-    }
-
     public static double getPercentage(double part, double whole) {
         return new BigDecimal(part * 100 / whole).doubleValue();
-    }
-
-    /**
-     * Méthode pour traduire un string et ajouter ses valeurs dans un objet Route
-     * 
-     * @param route: Objet Route à modifier
-     * @param data:  String à traduire
-     */
-    private void stringToRoute(Route route, String data) {
-        String[] splited = data.split(" ");
-        route.setDistance(Double.parseDouble(splited[0]));
-        route.setTripTime(Double.parseDouble(splited[1]));
-        route.setWaitingTime(Double.parseDouble(splited[2]));
-        route.setChargingTime(Double.parseDouble(splited[3]));
     }
 
     /**
@@ -280,133 +230,5 @@ public class VehicleService {
         value = value * factor;
         long tmp = Math.round(value);
         return (double) tmp / factor;
-    }
-
-    // --------------------------------- MÉTHODE TEMPORAIRE POUR TESTER L'ALGORITHME
-    // ---------------------------------
-
-    /**
-     * Méthode pour retourné une note aléatoire a chaque véhicule afin de tester le
-     * frontend. Temporaire
-     * 
-     * @return List de voiture avec une note aléatoire.
-     */
-    public List<Evaluation> dummyScore() {
-        List<Vehicle> list = getAllVehicle();
-        List<Evaluation> list2 = new ArrayList<>();
-        for (Vehicle vehicle : list) {
-            Evaluation eval = new Evaluation(vehicle);
-            list2.add(eval);
-        }
-
-        return list2;
-    }
-
-    /**
-     * Méthode pour testé l'algorithme avec un input de déplacement controllé.
-     * Temporaire.
-     * 
-     * @return
-     * @throws IOException
-     * @throws JSchException
-     * @throws InterruptedException
-     */
-    public List<Evaluation> evaluateVehicleTest() throws IOException, JSchException, InterruptedException {
-        List<Deplacement> coordinateList = createDeplacementList();
-        adveConnection.connectServer();
-        // -------- DÉBUT ALGO RÉEL---------
-        ArrayList<Route> routeList = new ArrayList<>();
-        ArrayList<Evaluation> vehicleFinalScore = new ArrayList<>();
-        int frequenceTotale = 0;
-        // --------Détermination de la fréquence total et du poid de chaque route
-        for (Deplacement x : coordinateList) {
-            Route route = new Route();
-            route.setFrequence(x.getFd().getFreq());
-            route.setDeplacement(x);
-            frequenceTotale += route.getFrequence();
-            routeList.add(route);
-        }
-        for (Route route : routeList) {
-            route.setWeight(getPercentage(route.getFrequence(), frequenceTotale));
-        }
-
-        // --------Évaluation de chaque route pour chaque voiture et calcule de la note
-        // final
-        List<Vehicle> allVehicle = getAllVehicle();
-        allVehicle.sort(Comparator.comparing(Vehicle::getElectricalCapacity));
-        for (int i = 0; i < allVehicle.size(); i++) {
-            double score = 0;
-            for (Route route : routeList) {
-                // --------Obtien les infos du déplacement avec la boite noir
-                String data = adveConnection
-                        .doRequest(requeteString(route) + allVehicle.get(i).getElectricalCapacity() * 1000);
-                System.out.println("Call Vehicle[" + (i + 1) + "] -> data: " + data);
-                // --------Donne une note au déplacement pour la voiture i
-                if (data.contains("Impossible")) {
-                    route.setScore(0);
-                } else {
-                    stringToRoute(route, data);
-                    evaluateRoute(route, allVehicle, i);
-                }
-            }
-            // -------- Calcule la note final de la voiture selon la note de chaque
-            // déplacement
-            for (Route route : routeList)
-                score = score + ((route.getWeight() / 100) * route.getScore());
-            // --------Ajoute le score final a la voiture et l'ajoute dans la liste a
-            // retourné
-            Evaluation evaluation = new Evaluation(allVehicle.get(i));
-            evaluation.setScore(round(score, 2));
-            vehicleFinalScore.add(evaluation);
-        }
-        adveConnection.closeServer();
-        // --------Sort les voitures par score
-        vehicleFinalScore.sort(Collections.reverseOrder(Comparator.comparing(Evaluation::getScore)));
-        return vehicleFinalScore;
-    }
-
-    private List<Deplacement> createDeplacementList() {
-        List<Deplacement> deplacementList = new ArrayList<>();
-        // normal -----------------------------------------------------------
-        deplacementList.add(
-                new Deplacement(1, new GeoPoint("dummy1", 45.1138, -72.3623), new GeoPoint("dummy2", 45.5382, -73.9159),
-                        new Trip()));
-        deplacementList.add(
-                new Deplacement(1, new GeoPoint("dummy3", 48.0293, -71.7262), new GeoPoint("dummy4", 45.0393, -72.5376),
-                        new Trip()));
-        deplacementList.add(
-                new Deplacement(1, new GeoPoint("dummy5", 47.6861, -70.3343), new GeoPoint("dummy6", 48.2191, -68.9323),
-                        new Trip()));
-        /*
-         * //Min --------------------------------------------------------------
-         * deplacementList.add(new Deplacement(1, new PointGeo(45.51963513223519,
-         * -73.64121842695846), new PointGeo(45.51934642873574, -73.64017685359295),
-         * FrequenceDeplacement.ONCE_A_YEAR));
-         * deplacementList.add(new Deplacement(1, new PointGeo(45.568363215529445,
-         * -73.57952489894289), new PointGeo(45.56722615890229, -73.57574873076257),
-         * FrequenceDeplacement.ONCE_A_YEAR));
-         * 
-         * //Max --------------------------------------------------------------
-         * deplacementList.add(new Deplacement(1, new PointGeo(45.404567768292274,
-         * -73.9545708308814), new PointGeo(45.7016485378072, -73.47982044062513),
-         * FrequenceDeplacement.TWICE_A_YEAR));
-         * deplacementList.add(new Deplacement(1, new PointGeo(45.701810104949196,
-         * -73.47925907001209), new PointGeo(45.40608420341184, -73.93127599467023),
-         * FrequenceDeplacement.ONCE_A_YEAR));
-         * 
-         * //Max Quebec -------------------------------------------------------
-         * deplacementList.add(new Deplacement(1, new PointGeo(45.406849308752534,
-         * -73.95165370074388), new PointGeo(48.416104487551735, -71.070981205327),
-         * FrequenceDeplacement.TWICE_A_YEAR));
-         * deplacementList.add(new Deplacement(1, new PointGeo(48.829245888782516,
-         * -64.48377519433731), new PointGeo(45.461830145587854, -75.69938395612203),
-         * FrequenceDeplacement.ONCE_A_WEEK));
-         * 
-         * //Impossible pour tous ---------------------------------------------
-         * deplacementList.add(new Deplacement(1, new PointGeo(61.59780702431485,
-         * -71.9571001824064), new PointGeo(45.461830145587854, -75.69938395612203),
-         * FrequenceDeplacement.ONCE_A_WEEK));
-         */
-        return deplacementList;
     }
 }
